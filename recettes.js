c37cbbfeea13c01ad2ef021d6c15e0c2fcc7fb81
Alec Montrade <alecmontrade@gmail.com>
2018-03-29 12:21:58 +0200

    Affichaques des recettes terminé

--- recettes.js
+++ recettes.js
@@ -21,7 +21,8 @@ function disp(){
         
         
         var recettes = document.createElement("div");
-        recettes.setAttribute("class", "recettes");
+        recettes.setAttribute("class", "recettes ");
+        recettes.setAttribute("style", "margin: 0 auto; width: 80%;");
 	console.log(doc_ici);
         for(var i = 0 ; i<3 ; i++){
             
@@ -30,15 +31,30 @@ function disp(){
             var recette = document.createElement("div");
             var recetteTexte = document.createElement("div");
             
-            recette.setAttribute("class", "card text-center col-md-6");
-            recette.setAttribute("style", "width: 80%; ");
+            recette.setAttribute("class", "card text-center ");
+            recette.setAttribute("style", "");
             recetteTexte.setAttribute("class", "card-body");
             
             
             var recetteTitre = document.createElement("h5");
-            recetteTitre.setAttribute("class", "card-title");
+            recetteTitre.setAttribute("class", "card-title display-2");
             recetteTitre.textContent=marecette.titre;
             recetteTexte.appendChild(recetteTitre);
+            
+            var badgeprix=document.createElement("span");
+            badgeprix.setAttribute("class","badge badge-danger");
+            badgeprix.textContent=marecette.prix;
+            recetteTexte.appendChild(badgeprix);
+
+            var badgetemps=document.createElement("span");
+            badgetemps.setAttribute("class","badge badge-primary");
+            badgetemps.textContent=marecette.tps+" min";
+            recetteTexte.appendChild(badgetemps);
+
+            var badgedifficulte=document.createElement("span");
+            badgedifficulte.setAttribute("class","badge badge-secondary");
+            badgedifficulte.textContent=marecette.difficulte;
+            recetteTexte.appendChild(badgedifficulte);
             
             var recettePar = document.createElement("p");
             recettePar.setAttribute("class", "card-text");
@@ -54,7 +70,7 @@ function disp(){
             recette.appendChild(recetteImg);
             
             var recetteBtn = document.createElement("a");
-            recetteBtn.setAttribute("class", "btn btn-primary");
+            recetteBtn.setAttribute("class", "btn btn-outline-danger");
             recetteBtn.setAttribute("href", "recette.html?id="+marecette.id);
             recetteBtn.textContent="Voir la recette";
             recette.appendChild(recetteBtn);
