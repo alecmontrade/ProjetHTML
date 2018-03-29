03546130fd494bb238ffbf97dbf288d2dc9b0dca
Alec Montrade <alecmontrade@gmail.com>
2018-03-29 12:27:16 +0200

    Affichage des articles selon toutes les recettes

--- recettes.js
+++ recettes.js
@@ -24,9 +24,8 @@ function disp(){
         recettes.setAttribute("class", "recettes ");
         recettes.setAttribute("style", "margin: 0 auto; width: 80%;");
 	console.log(doc_ici);
-        for(var i = 0 ; i<3 ; i++){
+        doc_ici.forEach(function(e){
             
-            marecette=doc_ici[i];
             console.log(marecette);
             var recette = document.createElement("div");
             var recetteTexte = document.createElement("div");
@@ -38,40 +37,40 @@ function disp(){
             
             var recetteTitre = document.createElement("h5");
             recetteTitre.setAttribute("class", "card-title display-2");
-            recetteTitre.textContent=marecette.titre;
+            recetteTitre.textContent=e.titre;
             recetteTexte.appendChild(recetteTitre);
             
             var badgeprix=document.createElement("span");
             badgeprix.setAttribute("class","badge badge-danger");
-            badgeprix.textContent=marecette.prix;
+            badgeprix.textContent=e.prix;
             recetteTexte.appendChild(badgeprix);
 
             var badgetemps=document.createElement("span");
             badgetemps.setAttribute("class","badge badge-primary");
-            badgetemps.textContent=marecette.tps+" min";
+            badgetemps.textContent=e.tps+" min";
             recetteTexte.appendChild(badgetemps);
 
             var badgedifficulte=document.createElement("span");
             badgedifficulte.setAttribute("class","badge badge-secondary");
-            badgedifficulte.textContent=marecette.difficulte;
+            badgedifficulte.textContent=e.difficulte;
             recetteTexte.appendChild(badgedifficulte);
             
             var recettePar = document.createElement("p");
             recettePar.setAttribute("class", "card-text");
-            recettePar.textContent=marecette.enTete;
+            recettePar.textContent=e.enTete;
             recetteTexte.appendChild(recettePar);
             
             recette.appendChild(recetteTexte);
             
             var recetteImg = document.createElement("img");
             recetteImg.setAttribute("class", "card-img-bottom");
-            recetteImg.setAttribute("src", marecette.img);
-            recetteImg.setAttribute("alt", marecette.titre);
+            recetteImg.setAttribute("src", e.img);
+            recetteImg.setAttribute("alt", e.titre);
             recette.appendChild(recetteImg);
             
             var recetteBtn = document.createElement("a");
             recetteBtn.setAttribute("class", "btn btn-outline-danger");
-            recetteBtn.setAttribute("href", "recette.html?id="+marecette.id);
+            recetteBtn.setAttribute("href", "recette.html?id="+e.id);
             recetteBtn.textContent="Voir la recette";
             recette.appendChild(recetteBtn);
                
@@ -81,14 +80,8 @@ function disp(){
             var br=document.createElement("BR");
             recettes.appendChild(br);
             
-            
-
-            
-                    
-		
-                //cpt++;
-		
-	}
+        })
+        
         
         document.body.appendChild(recettes);
         
